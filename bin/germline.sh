a0647f52765227a590f65c33694f2e00d5d0cbe2
nkblab-genomics <nkblabgenomics@gmail.com>
2024-11-27 16:12:53 +0530

    Update germline.sh

--- bin/germline.sh
+++ bin/germline.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Dependency: java-8, GATK-4
 
+Thisdir=</Path for analysis>
+cd "$Thisdir"
+
+REF=</Reference genome fasta>
+GATK_JAR=gatk-package-4.x.x.x-local.jar 
+
+# add germline g.vcf.gz for all samples, the example is provided for two samples.
+java -Xms22g -Xmx23g -jar "$GATK_JAR" CombineGVCFs \
+ -R "$REF" \
+ --variant </sample1.g.vcf.gz> \
+ --variant </sample2.g.vcf.gz> \
+ -O cohort.joint.g.vcf.gz
+
+java -Xms22g -Xmx23g -jar "$GATK_JAR" GenotypeGVCFs \
+ -R "$REF" \
+ -V cohort.joint.g.vcf.gz \
+ -O cohort.joint.vcf.gz
