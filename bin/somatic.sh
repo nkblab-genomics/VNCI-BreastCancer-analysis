2ec3244f8d74a59e277e446c44bb986e221ad550
nkblab-genomics <nkblabgenomics@gmail.com>
2024-11-27 16:14:27 +0530

    Update somatic.sh

--- bin/somatic.sh
+++ bin/somatic.sh
@@ -3,7 +3,7 @@
 Thisdir=</Directory for analysis>
 cd "$Thisdir"
 
-# Dependency: java-8, gatk-3.8-1-0 and 4.0.11.0, bcftools-1.9
+# Dependency: java-8, gatk-3.8-1-0 and >=4, bcftools-1.9
 # For this project chrY is excluded as all individuals are female
 
 REF=</Reference fasta file>
@@ -17,6 +17,7 @@ BAM_Tumor="$5"
 
 mkdir -p TMP_DIR
 
+# run using GATK>=4
 gatk --java-options "-Xms14g -Xmx16g" Mutect2 \
 -R $REF \
 -I $BAM_Tumor \
@@ -37,6 +38,7 @@ gatk --java-options "-Xms14g -Xmx16g" FilterMutectCalls \
 -O "$Sample_id".mutect2_filter.vcf.gz \
 -XL chrY
 
+# run using GATK==3.8-1-0
 java -jar -Xmx4g GenomeAnalysisTK-3.8-1-0/GenomeAnalysisTK.jar -T VariantsToAllelicPrimitives -R $REF -V "$Sample_id".mutect2_filter.vcf.gz -o "$Thisdir"/"$Sample_id"_temp1.vcf
 bcftools norm --check-ref e --fasta-ref $REF --multiallelics '-both' --output-type v "$Sample_id"_temp1.vcf > "$Thisdir"/"$Sample_id"_mutect2_normalised.vcf
 rm -f "$Thisdir"/"$Sample_id"_temp1.vcf*
